523b7be363ae1db8feb27092035b202c65ad8f12
Kei Mizubuchi <kmizubuc@my.centennialcollege.ca>
2020-02-07 10:48:47 -0500

    fixed gitignore error

---
+++ Scripts/util/constants.ts
@@ -0,0 +1,15 @@
+module util {
+    // image assets
+    export const BACKGROUND_PATH: string = './Assets/images/first-screen.png';
+
+    // Image Reference: https://pngimage.net/game-play-button-png-2/
+    export const PLAY_BUTTON: string = './Assets/images/play-btn.png';
+
+    // 
+    export const STAGE_W: number = 960;
+    export const STAGE_H: number = 640;
+
+    export const PLAYER_A_POS: objects.Vector2 = new objects.Vector2(50, 75);
+    export const PLAYER_B_POS: objects.Vector2 = new objects.Vector2(900, 600);
+
+}
