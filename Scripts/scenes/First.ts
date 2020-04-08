671c32803acbe69f5cd6bf7804bddfdd70d6ec98
Logan Junhwi Kim <logan.kim0212@gmail.com>
2020-04-07 21:37:40 -0400

    bullet to bullet collision animation bug fixed

--- Scripts/scenes/First.ts
+++ Scripts/scenes/First.ts
@@ -303,6 +303,7 @@ module scenes {
           if (destructableB[j].isColliding) {
             let explosion = new objects.Explosion(destructableA[i].x + destructableA[i].halfWidth, destructableA[i].y);
             this.addChild(explosion);
+            
             this.removeChild(destructableA[i]); // remove the bullet from the stage
             destructableA.splice(i, 1); // remove the bullet from the list
             this.removeChild(destructableB[j]); // remove the bullet from the stage
